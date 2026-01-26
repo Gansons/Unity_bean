169c56437cb358c1fc0ffda0f3c617f0b16c60cd
Gansons <gansonsraivis@gmail.com>
2026-01-26 12:12:31 +0200

    Bildes

---
+++ Assets/Scripts/NameScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NameScript : MonoBehaviour
+{
+    private string text;
+    private string[] input = {"Sveiks", "Jauku dienu", "Prieks Tevi redzēt", "Uzredzēšanos","Jauki, ka atnāci", "Tiksimies rīt"};
+    private int rand;
+    public GameObject inputField;
+    public GameObject textField;
+
+
+
+    public void GetText() 
+    { 
+        rand = Random.Range(0, input.Length);
+        text = inputField.GetComponent<TMP_InputField>().text;
+        textField.GetComponent<TMP_Text>().text = input[rand] + " " + text + "!";    
+    }
+}
